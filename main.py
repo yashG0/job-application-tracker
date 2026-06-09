464bb41a242efb736c10237eceb71e530c1551c2
yashG0 <yashgaurkar0@gmail.com>
2026-06-09 08:58:30 +0530

    added jinja and static files into fastapi app

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from api.application_route import application_routes
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
 app.include_router(application_routes)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
